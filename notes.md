3fe97b80d46acb761427df3ef324dfa7031cb6a5
vivek <kpvivekvettam@gmail.com>
2025-02-14 22:20:33 +0530

    DONE todo upto 18

--- notes.md
+++ notes.md
@@ -6,3 +6,11 @@
 
 templates from core/templates/core/base.html can access account/templates/account/signup.html
 via {% extends "core/base.html" %}
+
+- django have inbuilt authentication system
+- so if you can authicate use authicate directly
+- csrf token when doing with forms
+- login will directly create a session tokens and can validate accordingly
+-
+
+- after updateing views then should immedialtly take care of the urls.py also
--- studio/project/models.py
+++ studio/project/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 # Create your models here.
+import uuid
 
 from account.models import User
 
 class Project(models.Model):
-    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)    
+    name=models.CharField(max_length=255)
+    description=models.TextField(blank=True,null=True)
+    created_by=models.ForeignKey(User,related_name='projects',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+        
